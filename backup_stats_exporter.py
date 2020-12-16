a5f690c0ec1ffdfcabf1d3c11cddcadd04ee4a15
Yuriy Tabolin <yuriy.tabolin@jetbrains.com>
2020-12-16 11:44:10 +0300

    Added transfer size data to exporter

--- backup_stats_exporter.py
+++ backup_stats_exporter.py
@@ -14,6 +14,25 @@ backup_start_string = ''
 backup_end_string = ''
 backup_time = datetime.timedelta(seconds=0)
 
+
+def get_bytes(size):
+    if size == "0":
+        return int(0)
+    size_bytes = float(size[:-1])
+    suffix = size[-1]
+
+    if suffix == 'K':
+        return int(size_bytes * float(1024))
+    elif suffix == 'M':
+        return int(size_bytes * float(1024) * float(1024))
+    elif suffix == 'G':
+        return int(size_bytes * float(1024) * float(1024) * float(1024))
+    elif suffix == 'T':
+        return int(size_bytes * float(1024) * float(1024) * float(1024) * float(1024))
+
+    return 0
+
+
 def get_data_from_log_file():
 
     global buckets_list
@@ -29,6 +48,7 @@ def get_data_from_log_file():
     ## get last backup log from whole log file
     in_log_file = open('/var/log/backup_arh/minio_backup-last.log','r')
 
+    # take last_backup_log_reversed form whole log file
     last_backup_log_reversed = []
     last_log = 0
     for line in reversed(list(in_log_file)):
@@ -41,6 +61,7 @@ def get_data_from_log_file():
 
     in_log_file.close()
 
+    # create last_backup_log from last_backup_log_reversed
     last_backup_log = []
     for line in reversed(last_backup_log_reversed):
         last_backup_log.append(line)
@@ -72,10 +93,12 @@ def get_data_from_log_file():
             bucket_checked = 0
             bucket_checked_total = 0
 
+        if ("Transferred:" in line and "Bytes" in line):
+            bucket_transferred_bytes = re.search(r'[0-9]+\.{0,1}[0-9]*[KMGT]{0,1}', line).group(0)
+
         if "Transferred:" in line:
             bucket_transferred = re.search(r'[0-9]+', line).group(0)
             bucket_transferred_total = re.search(r'[0-9]+', re.split(r'[/]', line)[1]).group(0)
-
 
         if "Checks:" in line:
             bucket_checked = re.search(r'[0-9]+', line).group(0)
@@ -101,6 +124,7 @@ def get_data_from_log_file():
             bucket_dict['end_time'] = bucket_end_time_datetime
             bucket_dict['backup_time'] = bucket_time.seconds
             bucket_dict['transferred_files'] = bucket_transferred
+            bucket_dict['transferred_files_bytes'] = get_bytes(bucket_transferred_bytes)
             bucket_dict['checked_files'] = bucket_checked
             bucket_dict['transferred_total_files'] = bucket_transferred_total
             bucket_dict['checked_total_files'] = bucket_checked_total
@@ -116,6 +140,7 @@ last_backup_duration_time_metric = Gauge('last_backup_duration_time', 'Duration 
 
 bucket_backup_duration_times_metric = Gauge('bucket_backup_duration_times', 'Duration times of particular bucket backup', ['bucket_name'])
 bucket_backup_transferred_files_metric = Gauge('bucket_backup_transferred_files', 'Number of file, transferred while bucket backup', ['bucket_name'])
+bucket_backup_transferred_files_bytes_metric = Gauge('bucket_backup_transferred_files_bytes', 'Size of files (in bytes), transferred while bucket backup', ['bucket_name'])
 bucket_backup_checked_files_metric = Gauge('bucket_backup_checked_files', 'Number of file, checked while bucket backup', ['bucket_name'])
 bucket_backup_transferred_total_files_metric = Gauge('bucket_backup_transferred_total_files', 'Total number of files, needed to transfer while bucket backup', ['bucket_name'])
 bucket_backup_checked_total_files_metric = Gauge('bucket_backup_checked_total_files', 'Total number of files, needed to check while bucket backup', ['bucket_name'])
@@ -135,6 +160,7 @@ while True:
         #print(bucket['backup_time'])
         bucket_backup_duration_times_metric.labels(bucket['name']).set(bucket['backup_time'])
         bucket_backup_transferred_files_metric.labels(bucket['name']).set(bucket['transferred_files'])
+        bucket_backup_transferred_files_bytes_metric.labels(bucket['name']).set(bucket['transferred_files_bytes'])
         bucket_backup_checked_files_metric.labels(bucket['name']).set(bucket['checked_files'])
         bucket_backup_transferred_total_files_metric.labels(bucket['name']).set(bucket['transferred_total_files'])
         bucket_backup_checked_total_files_metric.labels(bucket['name']).set(bucket['checked_total_files'])
